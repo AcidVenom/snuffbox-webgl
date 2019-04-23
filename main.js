c56836bcccece3144dc2905e2d1869e399e2eb90
Daniel Konings <konings_304@hotmail.com>
2019-04-23 09:58:51 +0200

    - Cameras added

--- main.js
+++ main.js
@@ -3,12 +3,36 @@ import * as Snuff from "./lib/snuff-webgl.js"
 window.onload = function()
 {
     var app = new Snuff.Application("glCanvas");
-    var mesh, meshTransformA, meshTransformB, effect;
+    var mesh, meshTransformA, meshTransformB, effect, camera;
     var ready = false;
+
+    var heldKeys = [];
+
+    var KEY_W = 87;
+    var KEY_A = 65;
+    var KEY_S = 83;
+    var KEY_D = 68;
+
+    var KEY_LEFT = 37;
+    var KEY_UP = 38;
+    var KEY_RIGHT = 39;
+    var KEY_DOWN = 40;
+
+    document.addEventListener('keydown', function(e) 
+    {
+        heldKeys[e.keyCode] = true;
+    });
+
+    document.addEventListener("keyup", function(e)
+    {
+        heldKeys[e.keyCode] = false;
+    })
 
     var onInit = function()
     {
         var renderer = app.getRenderer();
+
+        camera = new Snuff.Camera();
 
         mesh = renderer.createMesh();
         
@@ -89,6 +113,10 @@ window.onload = function()
 
     var angle = 0.0;
     var time = 0.0;
+    var mx, mz;
+    var pitch = 0.0;
+    var yaw = 0.0;
+
     var onUpdate = function(dt)
     {
         time += dt;
@@ -105,6 +133,48 @@ window.onload = function()
         meshTransformB.setRotationEuler(angle * 2.0, 0.0, 0.0);
         var s = 0.5 + Math.abs(Math.sin(time) * 0.5);
         meshTransformA.setLocalScale(s, s, s);
+
+        mx = 0.0;
+        mz = 0.0;
+
+        if (heldKeys[KEY_W] === true)
+        {
+            mz = -1.0;
+        }
+        else if (heldKeys[KEY_S] === true)
+        {
+            mz = 1.0;
+        }
+
+        if (heldKeys[KEY_D] === true)
+        {
+            mx = 1.0;
+        }
+        else if (heldKeys[KEY_A] === true)
+        {
+            mx = -1.0;
+        }
+
+        if (heldKeys[KEY_LEFT] === true)
+        {
+            pitch += dt * 180.0;
+        }
+        else if (heldKeys[KEY_RIGHT] === true)
+        {
+            pitch -= dt * 180.0;
+        }
+
+        if (heldKeys[KEY_UP] === true)
+        {
+            yaw += dt * 180.0;
+        }
+        else if (heldKeys[KEY_DOWN] === true)
+        {
+            yaw -= dt * 180.0;
+        }
+
+        camera.translate(mx * 0.25, 0.0, mz * 0.25);
+        camera.setRotationEuler(yaw, pitch, 0.0);
     }
 
     var onDraw = function(renderer, dt)
@@ -114,8 +184,8 @@ window.onload = function()
             return;
         }
 
-        renderer.draw(meshTransformA, mesh, "Simple", "Default", "Default");
-        renderer.draw(meshTransformB, mesh, "Simple", "Default", "Default");
+        renderer.draw(camera, meshTransformA, mesh, effect, "Default", "Default");
+        renderer.draw(camera, meshTransformB, mesh, effect, "Default", "Default");
     }
 
     var errCode = app.exec(onInit, onUpdate, onDraw);
