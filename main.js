d0f168c5c483efc4faa34e8e2538a9893a8fd99e
Daniel Konings <konings_304@hotmail.com>
2019-05-12 17:16:47 +0200

    - More clean up, average FPS

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import * as Snuff from "./lib/snuff-webgl.js"
+import { DemoScene } from "./demo/demo_scene.js"
 
 window.onload = function()
 {
     var app = new Snuff.Application("glCanvas");
     var scene;
 
+    var avgDt = [];
+    var maxAvg = 100;
+
     var onInit = function()
     {
         var renderer = app.getRenderer();
-        scene = new Snuff.Scene(renderer);
+        scene = new DemoScene(renderer);
     }
 
     var onUpdate = function(dt)
@@ -18,6 +22,22 @@ window.onload = function()
 
     var onDraw = function(renderer, dt)
     {
+        avgDt.push(dt);
+        if (avgDt.length > maxAvg)
+        {
+            avgDt.splice(0, 1);
+        }
+
+        var avgFPS = 0;
+        for (var i = 0; i < avgDt.length; ++i)
+        {
+            avgFPS += avgDt[i];
+        }
+
+        avgFPS /= avgDt.length;
+        avgFPS = Math.floor(1.0 / avgFPS + 0.5);
+
+        document.querySelector("#fps").innerHTML = "<span>FPS: " + avgFPS + "</span>";
         scene.draw(dt);
     }
 
